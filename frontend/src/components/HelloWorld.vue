67f1e166f89f2c1a1336122cf0d3b0e5f9b8b120
narathip1598 <narathip@blendata.co>
2024-12-28 12:30:48 +0700

    Initial commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import API from '@/services/api';
+
+export default {
+  data() {
+    return {
+      message: ''
+    };
+  },
+  mounted() {
+    API.get('/')
+      .then(response => {
+        this.message = response.data.message;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+</script>
+
+<style scoped>
+</style>
